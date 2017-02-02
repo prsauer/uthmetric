337a5d13c0e63c8d96426e7858576377b0d22f9f
Ubuntu <ubuntu@ip-172-31-18-218.us-west-2.compute.internal>
2017-02-02 00:09:20 +0000

    mig

--- hello/apps/hellocount/views.py
+++ hello/apps/hellocount/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from hellocount.models import Visit
+#from hellocount.models import Visit
 
 def say_hello(request):
-    user_agent = request.META['HTTP_USER_AGENT']
-    Visit.objects.create(user_agent=user_agent)
-    return render(request, 'say_hello.html', {
-        'count': Visit.objects.count(),
-        'user_agent': user_agent
-    })
+	pass
+ #   user_agent = request.META['HTTP_USER_AGENT']
+##    Visit.objects.create(user_agent=user_agent)
+#    return render(request, 'say_hello.html', {
+#        'count': Visit.objects.count(),
+#        'user_agent': user_agent
+#    })
