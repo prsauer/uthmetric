f107036c1a9ae545688f913fab8ed38dd39f7dbc
Paul Sauer <paul@debitize.com>
2017-02-02 01:19:36 -0500

    A

--- hello/apps/hellocount/urls.py
+++ hello/apps/hellocount/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^player/(?P<rawname>[a-zA-Z]+)/$', views.get_by_name, name='player_by_name'),
     url(r'^leaders/(?P<realm>[a-zA-Z]+)/$', views.leaders, name='leaders'),
     url(r'^leaders/$', views.leaders, name='leaders_all'),
-    url(r'^render_leaders/$', views.leaders, name='render'),
+    url(r'^render_leaders/$', views.render_leaders, name='render'),
 ]
