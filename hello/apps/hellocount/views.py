7694513c60945343661ef64b8627ca6b6f14b9a4
Paul Sauer <paul@debitize.com>
2017-02-02 01:09:55 -0500

    A

--- hello/apps/hellocount/views.py
+++ hello/apps/hellocount/views.py
@@ -26,9 +26,9 @@ def leaders(request, realm=None):
 	if realm is not None and realm not in ["Albion","Midgard","Hibernia"]:
 		return HttpResponse("404")
 	if not realm:
-		players = Player.objects.all().order_by('-rps')[0:10].values()
+		players = Player.objects.all().order_by('-rps')[0:25].values()
 	else:
-		players = Player.objects.all().order_by('-rps').filter(realmname=realm)[0:10].values()
+		players = Player.objects.all().order_by('-rps').filter(realmname=realm)[0:25].values()
 
 	cdict =  {'realm': realm, 'players': players}
 
