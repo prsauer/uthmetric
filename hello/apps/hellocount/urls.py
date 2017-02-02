82f5b9bc512ab9991a654ac3b239810453a920f2
Paul Sauer <paul@debitize.com>
2017-02-01 23:16:06 -0500

    A

--- hello/apps/hellocount/urls.py
+++ hello/apps/hellocount/urls.py
@@ -3,5 +3,5 @@ from hellocount import views
 
 urlpatterns = [
     url(r'^$', views.post_data, name='home'),
-    url(r'^player/(?P<rawname>)[a-zA-Z]+/$', views.get_by_name, name='player_by_name')
+    url(r'^player/(?P<rawname>[a-zA-Z]+)/$', views.get_by_name, name='player_by_name')
 ]
