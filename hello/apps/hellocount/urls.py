3951fa6801d8395036819f62da4d976f024ed0a5
Paul Sauer <paul@debitize.com>
2017-02-01 23:59:49 -0500

    A

--- hello/apps/hellocount/urls.py
+++ hello/apps/hellocount/urls.py
@@ -3,5 +3,6 @@ from hellocount import views
 
 urlpatterns = [
     url(r'^$', views.post_data, name='home'),
-    url(r'^player/(?P<rawname>[a-zA-Z]+)/$', views.get_by_name, name='player_by_name')
+    url(r'^player/(?P<rawname>[a-zA-Z]+)/$', views.get_by_name, name='player_by_name'),
+    url(r'^leaders/(?P<realm>[a-zA-Z]+)/$', views.leaders, name='leaders'),
 ]
--- hello/apps/hellocount/views.py
+++ hello/apps/hellocount/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from hellocount.models import Player
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, JsonResponse
-import json
+from django.template.response import TemplateResponse
 
-import logging
+import json, logging
+
 logger = logging.getLogger('django')
 
 @csrf_exempt
@@ -19,9 +20,12 @@ def post_data(request):
 	p.update_from_json(jdata)
 	return HttpResponse("")
 
+def leaders(request, realm):
+	players = Player.objects.all()[0:10]
+	return TemplateResponse(request, 'leaders.html', {'players': players})
+
 @csrf_exempt
 def get_by_name(request, rawname):
-	logger.info("Req: %s"%rawname)
 	try:
 		p = Player.objects.get(rawname=rawname)
 		return JsonResponse(json.loads(p.raw_data))
