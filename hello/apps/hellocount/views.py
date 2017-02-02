11941fd9375bd240aa0fe5dec473cc97cec12ade
Paul Sauer <paul@debitize.com>
2017-02-01 19:33:42 -0500

    m

--- hello/apps/hellocount/views.py
+++ hello/apps/hellocount/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-#from hellocount.models import Visit
 from hellocount.models import Player
+from django.views.decorators.csrf import csrf_exempt
 
 import logging
 logger = logging.getLogger('django')
 
+@csrf_exempt
 def say_hello(request):
 	logger.info(request.POST)
 	f = open('/srv/output.txt', 'w')
