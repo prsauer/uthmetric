9818c6e82d751687cf3b1e7008d2a43594cab232
Paul Sauer <paul@debitize.com>
2017-02-01 19:17:15 -0500

    view

--- hello/apps/hellocount/views.py
+++ hello/apps/hellocount/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 #from hellocount.models import Visit
+from hellocount.models import Player
+
+import logging
+logger = logging.getLogger('django')
 
 def say_hello(request):
-	pass
- #   user_agent = request.META['HTTP_USER_AGENT']
-##    Visit.objects.create(user_agent=user_agent)
-#    return render(request, 'say_hello.html', {
-#        'count': Visit.objects.count(),
-#        'user_agent': user_agent
-#    })
+	logger.info(request.POST)
+
