30c3aa317d573a4206bb4a234e357b5ad726287c
Paul Sauer <paul@debitize.com>
2017-02-04 16:07:16 -0500

    A

--- hello/apps/hellocount/management/commands/update_all.py
+++ hello/apps/hellocount/management/commands/update_all.py
@@ -5,5 +5,14 @@ import time
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
+        total = Player.objects.all().count()
+        print "Updating",total,"characters"
+        count = 0
+        refreshed = 0
         for p in Player.objects.order_by('-rps'):
-            p.refresh_from_uth()
+            if p.refresh_from_uth():
+                refreshed += 1
+            count += 1
+            if count%100=0:
+                print 100.0*count/total
+        print "refreshed",refreshed,100.0*refreshed/total
