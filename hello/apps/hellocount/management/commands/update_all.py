26e480b7e1ae660e285ff1732f2a2c31cadb9430
Paul Sauer <paul@debitize.com>
2017-02-15 21:03:50 -0500

    Use new dump api

--- hello/apps/hellocount/management/commands/update_all.py
+++ hello/apps/hellocount/management/commands/update_all.py
@@ -1,24 +1,13 @@
 from django.core.management.base import BaseCommand, CommandError
 from hellocount.models import Player
-import time
+import time,requests,json
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        total = Player.objects.all().count()
-        print "Updating",total,"characters"
-        count = 0
-        refreshed = 0
-        for p in Player.objects.order_by('-rps'):
-            count += 1
-            if count%100 == 0:
-                print 100.0*count/total
-            if p.age() < 6:
-                continue
-            if p.level < 20 and p.age() < 24:
-                continue
-            if p.level < 40 and p.age() < 12:
-                continue
-            if p.refresh_from_uth():
-                refreshed += 1
-        print "refreshed",refreshed,100.0*refreshed/total
+        alldata = requests.get('https://uthgard.org/herald/api/dump')
+        jdata = json.loads(alldata.content)
+        for d in jdata:
+            n = d['Name']
+            p = Player.objects.get_or_create(rawname=n)
+            p.update_from_json(d)
