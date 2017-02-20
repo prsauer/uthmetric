75bc426940de9248290805b16c41ad9a1a7ddc8c
Paul Sauer <paul@debitize.com>
2017-02-20 15:59:38 -0500

    fix realmwar json

--- hello/apps/hellocount/views.py
+++ hello/apps/hellocount/views.py
@@ -151,11 +151,11 @@ def realmwarjson(request):
 	realm_keeps.append({'realm': 'Midgard', 'keeps': Keep.objects.filter(location="Midgard").order_by('name')})
 	realm_keeps.append({'realm': 'Hibernia', 'keeps': Keep.objects.filter(location="Hibernia").order_by('name')})
 	enc = MasterEncoder()
-	return JsonResponse(enc.encode({'realm': 'realmwar',
+	return JsonResponse(json.loads(enc.encode({'realm': 'realmwar',
 					   'all_keeps': Keep.objects.all(),
 					   'realm_keeps': realm_keeps,
 					   'timestamp': most_recent(),
-					   'df': DFalls.objects.first()}))
+					   'df': DFalls.objects.first()})))
 
 def realmwar2(request):
 	alb = ["alb"]*7
