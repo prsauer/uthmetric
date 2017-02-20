2797d2a50032146e18813254e9b68316cc4ce652
Paul Sauer <paul@debitize.com>
2017-02-20 13:02:32 -0500

    One star too few

--- hello/apps/hellocount/views.py
+++ hello/apps/hellocount/views.py
@@ -56,7 +56,7 @@ def leaders_api(request):
 	the_args = {}
 	if request.GET.get('class'):
 		the_args['classname'] = single_case(request.GET.get('class'))
-	res = Player.objects.filter(rps__isnull=False).filter(*the_args).order_by('-rps')[0:25]
+	res = Player.objects.filter(rps__isnull=False).filter(**the_args).order_by('-rps')[0:25]
 	enc = MasterEncoder()
 	return JsonResponse(json.loads(enc.encode({'input': ins, 'results': res})))
 
