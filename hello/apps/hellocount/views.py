b5fdd10c3fd16a1d4fe87290bb3434d8d28578ff
Paul Sauer <paul@debitize.com>
2017-02-28 12:15:51 -0500

    Linkages to weekly stuff

--- hello/apps/hellocount/views.py
+++ hello/apps/hellocount/views.py
@@ -157,6 +157,7 @@ def weekly_solo_leaders(request):
 
 def weekly_realm_leaders(request):
 	cdict = {}
+	cdict['realm'] = 'weekly_realm'
 	cdict['alb_7'] = Player.objects.filter(realmname="Albion").aggregate(total_rps=Sum('rps_last7'))
 	cdict['hib_7'] = Player.objects.filter(realmname="Hibernia").aggregate(total_rps=Sum('rps_last7'))
 	cdict['mid_7'] = Player.objects.filter(realmname="Midgard").aggregate(total_rps=Sum('rps_last7'))
