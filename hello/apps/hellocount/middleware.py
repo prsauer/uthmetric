3359bd703901f005f2fc96382aca1c37f3e00e4b
Paul Sauer <paul@debitize.com>
2017-05-23 13:19:39 -0400

    bufix

--- hello/apps/hellocount/middleware.py
+++ hello/apps/hellocount/middleware.py
@@ -4,6 +4,7 @@ from django.middleware.cache import UpdateCacheMiddleware
 
 
 class SmartUpdateCacheMiddleware(UpdateCacheMiddleware):
+    STRIP_RE = re.compile(r'\b(_[^=]+=.+?(?:; |$))')
     def process_request(self, request):
         cookie = self.STRIP_RE.sub('', request.META.get('HTTP_COOKIE', ''))   
         request.META['HTTP_COOKIE'] = cookie
